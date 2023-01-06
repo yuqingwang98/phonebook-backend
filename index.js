c58acdc6d44d9f439a6f43f4b442de32d2647017
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-06 09:00:56 +0000

    step 3 working

--- index.js
+++ index.js
@@ -46,6 +46,17 @@ app.get('/info', (request, response) => {
   <p>${currentDate} (${timeZone})</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = persons.find(person => person.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
