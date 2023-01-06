e2e3f0e9e0b6fdaa4f92a9c365b993530a038694
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-06 09:31:38 +0000

    step5 working; making POST requests with random id

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "id": 1,
@@ -55,6 +57,41 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return Math.floor(Math.random() * (1000 - maxId) + maxId)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  console.log(request)
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  console.log(person)
+
+  persons = persons.concat(person)
+
+  response.json(persons)
 })
 
 const PORT = 3001
