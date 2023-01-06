0a0c1fef909a287a4e03a1b8d7c04f66052d0548
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-06 00:43:40 +0000

    step 2 done on metropolitan line; showing time with timezone

--- index.js
+++ index.js
@@ -24,12 +24,26 @@ let persons = [
   }
 ]
 
+let info =
+  [
+    {
+      date: new Date()
+    }
+ ]
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const currentDate = new Date().toLocaleString();
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p>${currentDate} (${timeZone})</p>`)
 })
 
 const PORT = 3001
